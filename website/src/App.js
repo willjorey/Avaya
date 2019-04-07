da68e22d83d366e45e102f3585b0b95a6978943a
willjorey <william.j.schober.reyes@gmail.com>
2019-04-07 13:08:21 -0400

    Can now view connections in a table with sorting

--- website/src/App.js
+++ website/src/App.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
+import BootstrapTable from 'react-bootstrap-table-next';
+
 class App extends Component {
   state={
     connections: []
@@ -10,10 +14,33 @@ class App extends Component {
     var json = await response.json();
     var connections = json.connections;
     this.setState({connections});
-    console.log(connections)
+    console.log(connections);
   }
   
   render() {
+    var columns = [{
+      dataField:'names[0].displayName',
+      text: 'Name',
+      sort: true
+    },
+    {
+      dataField: 'birthdays[0].text',
+      text:'Birthday',
+      sort: true
+    },
+    {
+      dataField: 'genders[0].formattedValue',
+      text:'Gender',
+      sort: true
+    }
+    ];
+    var rowEvents={
+      onClick: (e, row, rowIndex)=>{
+        console.log('EVENT', e);
+        console.log('ROW', row);
+        console.log('INDEX', rowIndex);
+      }
+    }
     return (
       <div className="App">
         <p>
@@ -22,6 +49,9 @@ class App extends Component {
         <button onClick={() => this.onGetConnections()}>
           Get Connections
         </button>
+        <div>
+          <BootstrapTable keyField='resourceName' data={this.state.connections} columns={columns} rowEvents={ rowEvents } striped hover condensed />
+        </div>
       </div>
     );
   }
